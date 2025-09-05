6cc956d5132d88a5d06a864f9d5065d75a0871df
Ketan narhe <ketannarhe02@gmail.com>
2025-09-05 17:09:06 +0530

    Add files via upload

---
+++ ExtractChar.java
@@ -0,0 +1,17 @@
+package String;
+
+public class ExtractChar {
+
+	public static void main(String[] args) {
+		
+		
+		String name="ketanNarhe";
+
+		for(char i=0;i<name.length();i++)
+			
+		{
+			System.out.println(name.charAt(i));
+		}
+	}
+
+}
